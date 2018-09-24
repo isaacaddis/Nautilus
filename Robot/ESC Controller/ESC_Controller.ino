db27f56df98d539d58315af2ca0f51dc2a3f31c7
Alex Vazquez <vazquezalex1965@gmail.com>
2018-09-24 04:56:44 +0000

    ESC_Controller.ino edited online with Bitbucket

--- Robot/ESC Controller/ESC_Controller.ino
+++ Robot/ESC Controller/ESC_Controller.ino
@@ -6,44 +6,85 @@
  * 
  * Takes data from Surface Arduino and determine the ESC control  
  * 
- */
- #include <Servo.h>
- Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+ *
+ 
+ #include "Servo.h"
 
-int pos = 0;    // variable to store the servo position
+char sendsig[25]; //Initialized variable to store recieved data
 
+int speed01; 
+int speed02; 
+int speed03; 
+int luigiInt;
+Servo OutputM1; //Motor 1 FRD/BCK AND PLFT/PRGT AND LFT/RGT
+Servo OutputM2; //Motor 2 FRD/BCK AND PLFT/PRGT AND LFT/RGT
+Servo OutputM3; //Motor 3 FRD/BCK AND PLFT/PRGT AND LFT/RGT
+Servo OutputM4; //Motor 4 FRD/BCK AND PLFT/PRGT AND LFT/RGT
+Servo OutputM5; //Motor 5 UP/DOWN AND TILT
+Servo OutputM6; //Motor 6 UP/DOWN AND TILT
 
-char mystr[4]; //Initialized variable to store recieved data
-int testOutput = 0;
-String str2 = "10";
 void setup() {
-  myservo.attach(9); 
-// Begin the Serial at 9600 Baud
-Serial1.begin(9600);
-Serial.begin(9600);
-pinMode(LED_BUILTIN, OUTPUT);
+  // Begin the Serial at 9600 Baud
+  Serial.begin(9600);
+  OutputM1.attach(10);
+  OutputM2.attach(11);
+  OutputM3.attach(12);
+  OutputM4.attach(5);
+  OutputM5.attach(3);
+  OutputM6.attach(4);
 }
 
 void loop() {
 
-digitalWrite(LED_BUILTIN, LOW); 
-Serial1.readBytes(mystr,4); //Read the serial data and store in var
-String str(mystr);
-testOutput = str.toInt();
- if (testOutput>= 1 and testOutput<= 10) {
-    Serial.println("Forwards");  
-    pos = map(testOutput, 1, 10, 90, 165);
-  myservo.write(pos); 
- }
-  if (testOutput>= 11 and testOutput<= 20) {
-    Serial.println("Backwards"); 
-      pos = map(testOutput, 11, 20, 90, 24);
-  myservo.write(pos); 
- }
-   if (testOutput<= 1) {
-    Serial.println("NONE"); 
-  myservo.write(90); 
- }
-delay(100);
-}
+  Serial.readBytes(sendsig,25); 
+  String myString = String(sendsig);
+  String mario = myString.substring(7,9);
+  String luigi = myString.substring(10,12);
+  String zerO = "0";
+  int luigiInt = luigi.toInt();
+ 
+  
+  if (myString.charAt(0) == '-') { //PLeft
+    Serial.println("L" + mario);
+    speed01 = map(luigiInt, 1, 10, 90, 180);
+    speed02 = map(luigiInt, 1, 10, 90, 0);
+    OutputM1.write(speed01);
+    OutputM2.write(speed02);
+    OutputM3.write(speed02);
+    OutputM4.write(speed01);
+  }
+  if (myString.charAt(0) == '+') { //PRight
+    Serial.println("R" + mario);
+    speed01 = map(luigiInt, 1, 10, 90, 180);
+    speed02 = map(luigiInt, 1, 10, 90, 0);
+    OutputM1.write(speed02);
+    OutputM2.write(speed01);
+    OutputM3.write(speed01);
+    OutputM4.write(speed02);
+  }
+  if (myString.charAt(3) == '+') { //Forwards
+    Serial.println("F" + luigi);
+    speed01 = map(luigiInt, 1, 10, 90, 0);
+    OutputM1.write(speed01);
+    OutputM2.write(speed01);
+    OutputM3.write(speed01);
+    OutputM4.write(speed01);
+  }
+  if (myString.charAt(3) == '-') { //Backwards
+    Serial.println("B" + luigi);
+    speed01 = map(luigiInt, 1, 10, 90, 180);
+    OutputM1.write(speed01);
+    OutputM2.write(speed01);
+    OutputM3.write(speed01);
+    OutputM4.write(speed01);
+  }
+  if ((myString.charAt(0) == '0') && (myString.charAt(3) == '0')){
+    Serial.println("N" + zerO);
+    OutputM1.write(90);
+    OutputM2.write(90);
+    OutputM3.write(90);
+    OutputM4.write(90);
+    }
+    
+   delay(100);
+}/
