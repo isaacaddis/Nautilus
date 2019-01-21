d60a48fceb8f2f154f0f6385535149b23070ccea
Alex <vazquezalex1965@gmail.com>
2019-01-20 16:02:06 -0800

    modified:   Receiver/Receiver.ino 	modified:   Sender/Sender_updated_code__may_work_/Sender_updated_code__may_work_.ino

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -1,27 +1,137 @@
+////Receiver Code
+//
+//char str[4];
+//
+//void setup() {
+//  Serial.begin(9600);
+//  Serial1.begin(9600);
+//}
+//
+//void loop() {
+//  int i=0;
+//
+//  if (Serial1.available()) {
+//    delay(100); //allows all serial sent to be received together
+//    while(Serial1.available() && i<4) {
+//      str[i++] = Serial1.read();
+//    }
+//    str[i++]='\0';
+//  }
+//
+// if(i>0) {
+//    for(int j =0; j<4;j++) {
+//        Serial.print(str[j]);
+//    }
+//    Serial.println("");
+// }
+//}
+
 //Receiver Code
 
-char str[4];
+
+#include "Servo.h"
+
+char sendsig[35]; //Initialized variable to store recieved data
+char sending[35]; //Initialized variable to store recieved data
+
+
+
+int speed01; 
+int speed02; 
+int speed03; 
+int speed04;
+int luigiInt;
+int yoshiInt;
+Servo OutputM1; //Motor 1 FRD/BCK AND PLFT/PRGT AND LFT/RGT
+Servo OutputM2; //Motor 2 FRD/BCK AND PLFT/PRGT AND LFT/RGT
+Servo OutputM3; //Motor 3 FRD/BCK AND PLFT/PRGT AND LFT/RGT
+Servo OutputM4; //Motor 4 FRD/BCK AND PLFT/PRGT AND LFT/RGT
+Servo OutputM5; //Motor 5 UP/DOWN AND TILT
+Servo OutputM6; //Motor 6 UP/DOWN AND TILT
 
 void setup() {
+  // Begin the Serial at 9600 Baud
   Serial.begin(9600);
-  Serial1.begin(9600);
+  OutputM1.attach(10);
+  OutputM2.attach(11);
+  OutputM3.attach(12);
+  OutputM4.attach(5);
+  OutputM5.attach(3);
+  OutputM6.attach(4);
 }
 
 void loop() {
-  int i=0;
-
-  if (Serial1.available()) {
-    delay(100); //allows all serial sent to be received together
-    while(Serial1.available() && i<4) {
-      str[i++] = Serial1.read();
-    }
-    str[i++]='\0';
+  Serial.readBytes(sending,35);
+  Serial.println(sending);
+  
+  Serial.readBytes(sendsig,35); 
+  String myString = String(sendsig);
+  String mario = myString.substring(10,12);
+  String luigi = myString.substring(13,15);
+  String yoshi = myString.substring(16,18);
+  String zerO = "0";
+  int marioInt = mario.toInt();
+  int luigiInt = luigi.toInt();
+  int yoshiInt = yoshi.toInt(); 
+  
+  if (myString.charAt(0) == '-') { //PLeft
+    Serial.println("L" + mario);
+    speed01 = map(marioInt, 1, 10, 90, 180);
+    speed02 = map(marioInt, 1, 10, 90, 0);
+    OutputM1.write(speed01);
+    OutputM2.write(speed02);
+    OutputM3.write(speed02);
+    OutputM4.write(speed01);
+  }
+  if (myString.charAt(0) == '+') { //PRight
+    Serial.println("R" + mario);
+    speed01 = map(marioInt, 1, 10, 90, 180);
+    speed02 = map(marioInt, 1, 10, 90, 0);
+    OutputM1.write(speed02);
+    OutputM2.write(speed01);
+    OutputM3.write(speed01);
+    OutputM4.write(speed02);
+  }
+  if (myString.charAt(3) == '+') { //Forwards
+    Serial.println("F" + luigi);
+    speed01 = map(luigiInt, 1, 10, 90, 0);
+    OutputM1.write(speed01);
+    OutputM2.write(speed01);
+    OutputM3.write(speed01);
+    OutputM4.write(speed01);
+  }
+  if (myString.charAt(3) == '-') { //Backwards
+    Serial.println("B" + luigi);
+    speed01 = map(luigiInt, 1, 10, 90, 180);
+    OutputM1.write(speed01);
+    OutputM2.write(speed01);
+    OutputM3.write(speed01);
+    OutputM4.write(speed01);
+  }
+  if ((myString.charAt(0) == '0') && (myString.charAt(3) == '0')){
+    Serial.println("N" + zerO);
+    OutputM1.write(90);
+    OutputM2.write(90);
+    OutputM3.write(90);
+    OutputM4.write(90);
+  }
+  if (myString.charAt(6) == '+') { //Up
+    Serial.println("U" + yoshi);
+    speed03 = map(yoshiInt, 1, 10, 90, 180);
+    OutputM5.write(speed03);
+    OutputM6.write(speed03);
+  }
+  if (myString.charAt(6) == '-') { //Down
+    Serial.println("D" + yoshi);
+    speed03 = map(yoshiInt, 1, 10, 90, 0);
+    OutputM5.write(speed03);
+    OutputM6.write(speed03);
+  }
+   if ((myString.charAt(6) == '0') && (myString.charAt(15) == '0')){
+    Serial.println("N" + zerO);
+    OutputM5.write(90);
+    OutputM6.write(90);
   }
 
- if(i>0) {
-    for(int j =0; j<4;j++) {
-        Serial.print(str[j]);
-    }
-    Serial.println("");
- }
+  delay(100);
 }
