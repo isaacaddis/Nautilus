cba951bb9ca4a4aae0e8815de9600bd770d24de4
Alex Vazquez <vazquezalex1965@gmail.com>
2019-04-23 04:47:46 +0000

    Sender_updated_code__may_work_.ino edited online with Bitbucket

--- Sender/Sender_updated_code__may_work_/Sender_updated_code__may_work_.ino
+++ Sender/Sender_updated_code__may_work_/Sender_updated_code__may_work_.ino
@@ -27,7 +27,7 @@ int SpeedA4= 0;
 int rotate = 0;
 int PotA5;
 
-char data;
+String data;
 int outputValueX = 0;
 int outputValueY = 0;
 int outputValueZ = 0;
@@ -404,7 +404,7 @@ Serial1.write(joy,30);
 
 
     Serial1.readBytes(m_str,45);
-    data = m_str;
+    data =String(m_str);
     Serial.println(data+ ".\n");
     
     //String TempAndGyro = String(m_str);
