c72025113923080e430649486d557f9524f730c8
Alex Vazquez <vazquezalex1965@gmail.com>
2019-06-10 22:50:48 +0000

    camDisplay.py edited online with Bitbucket
    Former-commit-id: 5f8fdab4258b5a22b3e142b0167d63faeadbc303

--- Robot/Systems/Vision/camDisplay.py
+++ Robot/Systems/Vision/camDisplay.py
@@ -38,7 +38,8 @@ class Display():
         ret, val = self.cap.read()
         if val is not None:
             img_c  = val.copy()
-            crop_frame = val[0:int(val.shape[0]/1.52)]
+            #crop_frame = val[0:int(val.shape[0]/1.52)]
+            crop_frame = img_c
             #crop_frame = cv2.cvtColor(val, cv2.COLOR_BGR2GRAY)
             #crop_frame = self.proc.process(crop_frame)
             #cv2.imwrite('cropped.jpg',crop_frame)
@@ -94,27 +95,31 @@ class Display():
                     if area > max_n:
                         max_n = area
                     #print("Max area: {}".format(max_n))
-                    current_len += 1
-                    cv2.drawContours(img_c, [i], 0, (0,0,255))
-                    s = self.sd.detect(i,img_c)
-                    #print(s)
-                    if s == "triangle":
-                        self.n_text[0] += 1
-                    elif s == "square":
-                        self.n_text[1] += 1
-                    elif s == "line":
-                        self.n_text[2] += 1
-                    elif s == "circle":
-                        self.n_text[3] += 1 
-                    a = "Shapes: {}".format(current_len)
-                    b = "▲: {}".format(self.n_text[0])
-                    c = "■: {}".format(self.n_text[1])
-                    d = "▬ {}".format(self.n_text[2])
-                    e = "●: {}".format(self.n_text[3])
-            self.past_text = self.n_text
-            self.n_text = [0, 0, 0, 0, 0]
-            return (a,b,c,d,e, img_c)
-        return (None, None)
+                    rect = cv2.minAreaRect(c)
+                    box = cv2.boxPoints(rect)
+                    box = np.int0(box)
+                    within_bound = sd.check(box, 215, 440, 260)
+                    if (within_bound):
+                        current_len += 1
+                        cv2.drawContours(img_c, [i], 0, (0,0,255))
+                        s = self.sd.detect(i,img_c)
+                        if s == "triangle":
+                            self.n_text[0] += 1
+                        elif s == "square":
+                            self.n_text[1] += 1
+                        elif s == "line":
+                            self.n_text[2] += 1
+                        elif s == "circle":
+                            self.n_text[3] += 1 
+                        a = "Shapes: {}".format(current_len)
+                        b = "▲: {}".format(self.n_text[0])
+                        c = "■: {}".format(self.n_text[1])
+                        d = "▬ {}".format(self.n_text[2])
+                        e = "●: {}".format(self.n_text[3])
+                self.past_text = self.n_text
+                self.n_text = [0, 0, 0, 0, 0]
+                return (a,b,c,d,e, img_c)
+            return (None, None)
     def close(self):
         self.cap.release()
 if __name__ == "__main__":
