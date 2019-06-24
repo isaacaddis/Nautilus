dfeba4a734b6907fd89f2b3a0a3f01191a376c35
PhysicsAlex <vazquezalex1965@gmail.com>
2019-06-24 00:02:17 -0700

    Finalized, finished, refined vision
    Former-commit-id: 3067e1cef45c873f6740d365d21b11068ddb1044

--- Robot/Systems/Vision/camDisplay.py
+++ Robot/Systems/Vision/camDisplay.py
@@ -10,101 +10,121 @@ from . import Async
 from . import Undistort
 from . import methods
 import time
-import numpy as np
+import self.n_textpy as np
 import time
 import imutils
+
 
 class Display():
     def __init__(self, num=1):
         print(" Initializing Benthic Species ")
         self.num = num
-        self.cap = cv2.VideoCapture(num)
+        self.cap = cv2.VideoCapture(self.n_text)
         self.und = Undistort.Undistort()
         self.proc = ImagePreProcess.ImagePreProcess()
         self.sd = geo.ShapeDetect()
-        self.past_text, self.n_text = [0, 0, 0, 0, 0],[0, 0, 0, 0] # follows the general data schema for shapes
+        self.past_text, self.n_text = [0, 0, 0, 0, 0], [
+            0, 0, 0, 0]  # follows the general data schema for shapes
+        self.COLORS = [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)]
+        self.COLORS[0] = (0, 0, 255)
+        self.COLORS[1] = (255, 0,)
+        self.COLORS[2] = (255, 0, 255)
+        self.COLORS[3] = (0, 255,)
+        self.classes = None
         self.Contours = methods.Contours()
-        self.lower_black = np.array([0,0,0])
-        self.upper_black = np.array([0,0,255])
-        self.fgbg = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=50, detectShadows=True)
+        self.lower_black = np.array([0, 0, 0])
+        self.upper_black = np.array([0, 0, 255])
+        self.net = cv2.dnn.readNet(
+            '../darknet/shapes-tiny_150000.weights', '../darknet/cfg/shapes-tiny.cfg')
+        #self.fgbg = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=50, detectShadows=True)
 
         print("Finished Benthic Species Initalization")
+
+    def get_output_layers(self, net):
+        layer_names = net.getLayerNames()
+        output_layers = [layer_names[i[0] - 1]
+                         for i in net.getUnconnectedOutLayers()]
+        return output_layers
+
+    def draw_bounding_box(self, img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+        label = str(classes[class_id])
+        color = COLORS[class_id]
+        cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+        cv2.putText(img, label, (x-10, y-10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
     def get(self):
+        with open('../darknet/shapes.names', 'r') as f:
+            self.classes = [line.strip() for line in f.readLines()]
         a = ""
         b = ""
         c = ""
         d = ""
         e = ""
-        ret, val = self.cap.read()
-        if val is not None:
-            img_c  = val.copy()
-            crop_frame = img_c[0:int(img_c.shape[0]/1.58)]
-            rgb_planes = cv2.split(crop_frame)
-            result_planes = []
-            for plane in rgb_planes:
-                dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
-                #bg_img = cv2.medianBlur(dilated_img, 21)
-                bg_img = dilated_img
-                diff_img = 255 - cv2.absdiff(plane, bg_img)
-                result_planes.append(diff_img)
-
-            result = cv2.merge(result_planes)
-            ret,th1 = cv2.threshold(result,127,255,cv2.THRESH_BINARY_INV)
-            mask_img = self.Contours.applyMask(th1)
-            th, im_th = cv2.threshold(mask_img, 220, 255, cv2.THRESH_BINARY_INV)
-
-            # Copy the thresholded image.
-            im_floodfill = im_th.copy()
-
-            # Mask used to flood filling.
-            # Notice the size needs to be 2 pixels than the image.
-            h, w = im_th.shape[:2]
-            mask = np.zeros((h+2, w+2), np.uint8)
-
-            # Floodfill from point (0, 0)
-            cv2.floodFill(im_floodfill, mask, (0,0), 255);
-
-            # Invert floodfilled image
-            im_floodfill_inv = cv2.bitwise_not(im_floodfill)
-
-            # Combine the two images to get the foreground.
-            im_out = im_th | im_floodfill_inv
-            _,cnts,__ = cv2.findContours(im_out, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            """crop_frame = img_c
-            ret,thres = cv2.threshold(crop_frame,127,255,cv2.THRESH_BINARY_INV)
+        ret, image = self.cap.read()
+        if image is not None:
+            img_c = image.copy()
+            ret, thres = cv2.threshold(result, 127, 255, cv2.THRESH_BINARY_INV)
             mask_img = self.Contours.applyMask(thres)
-            __,cnts,_ = cv2.findContours(mask_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)"""
-            current_len = 0
-            max_n = 0
-            for i in cnts:
-                area = cv2.contourArea(i)
-                if area > 25 and area < 3200:
-                    if area > max_n:
-                        max_n = area
-                    #print("Max area: {}".format(max_n))
-                    current_len += 1
-                    cv2.drawContours(img_c, [i], 0, (0,0,255),cv2.FILLED)
-                    s = self.sd.detect(i,img_c)
-                    if s == "triangle":
-                        self.n_text[0] += 1
-                    elif s == "square":
-                        self.n_text[1] += 1
-                    elif s == "line":
-                        self.n_text[2] += 1
-                    elif s == "circle":
-                        self.n_text[3] += 1 
-                    a = "Shapes: {}".format(current_len)
-                    b = "▲: {}".format(self.n_text[0])
-                    c = "■: {}".format(self.n_text[1])
-                    d = "▬: {}".format(self.n_text[2])
-                    e = "●: {}".format(self.n_text[3])
+            th, im_th = cv2.threshold(
+                mask_img, 220, 255, cv2.THRESH_BINARY_INV)
+            image = im_th[0:int(im_th.shape[0]/1.5)]
+            Width = image.shape[1]
+            Height = image.shape[0]
+            scale = 0.00392
+            blob = cv2.dnn.blobFromImage(
+                image, scale, (416, 416), (0, 0, 0), True, crop=False)
+            self.net.setInput(blob)
+            outs = self.net.forward(self.get_output_layers(self.net))
+            class_ids = []
+            confidences = []
+            boxes = []
+            conf_threshold = 0.5
+            nms_threshold = 0.4
+            for out in outs:
+                for detection in out:
+                    scores = detection[5:]
+                    class_id = np.argmax(scores)
+                    confidence = scores[class_id]
+                    if confidence > 0.5:
+                        center_x = int(detection[0] * Width)
+                        center_y = int(detection[1] * Height)
+                        w = int(detection[2] * Width)
+                        h = int(detection[3] * Height)
+                        x = center_x - w / 2
+                        y = center_y - h / 2
+                        class_ids.append(class_id)
+                        confidences.append(float(confidence))
+                        boxes.append([x, y, w, h])
+            for i in indices:
+                i = i[0]
+                box = boxes[i]
+                x = box[0]
+                y = box[1]
+                w = box[2]
+                h = box[3]
+                # print(class_ids[i])
+                self.n_text[class_ids[i]] += 1
+                # print(self.n_text)
+                self.draw_bounding_box(image, class_ids[i], confidences[i], round(
+                    x), round(y), round(x+w), round(y+h))
+            indices = cv2.dnn.NMSBoxes(
+                boxes, confidences, conf_threshold, nms_threshold)
+            a = "Shapes: {}".format(current_len)
+            b = "▲: {}".format(self.n_text[0])
+            c = "■: {}".format(self.n_text[1])
+            d = "▬: {}".format(self.n_text[2])
+            e = "●: {}".format(self.n_text[3])
             self.past_text = self.n_text
             self.n_text = [0, 0, 0, 0]
-            #print(d)
-            return (a,b,c,d,e, img_c)
+            # print(d)
+            return (a, b, c, d, e, image)
         return (None, None)
+
     def close(self):
         self.cap.release()
+
+
 if __name__ == "__main__":
     d = Display()
     while True:
